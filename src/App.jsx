d8205a4f0a7641ba12750d6e42f0d6912606b942
Treasure Ajefu <treasureajefu@gmail.com>
2024-03-20 20:16:25 +0100

    pagination done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,91 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { useEffect, useState } from 'react'
+import questions from './data'
 
 function App() {
+  const [selectedTag, setSelectedTag] = useState('All')
+  const [filteredQuestions, setFilteredQuestions] = useState(questions);
+  const [currentPage, setCurrentPage] = useState(1);
+  const questionsPerPage = 10;
+
+  useEffect(() => {
+    if (selectedTag === 'All') {
+      setFilteredQuestions(questions);
+    } else {
+      const filtered = questions.filter(question =>
+        question.tags.includes(selectedTag)
+      );
+      setFilteredQuestions(filtered);
+    }
+    setCurrentPage(1)
+  }, [selectedTag, questions]);
+
+  const handleTagClick = tag => {
+    setSelectedTag(tag);
+  };
+
+  const renderTags = () => {
+    const allTags = ['All', ...new Set(questions.flatMap(q => q.tags))];
+    return allTags.map(tag => (
+      <span
+        key={tag}
+        onClick={() => handleTagClick(tag)}
+        className={`tag-button ${tag === selectedTag ? 'active' : ''}`}
+      >
+        {tag}
+      </span>
+    ));
+  };
+
+  // Pagination
+  const indexOfLastQuestion = currentPage * questionsPerPage;
+  const indexOfFirstQuestion = indexOfLastQuestion - questionsPerPage;
+  const currentQuestions = filteredQuestions.slice(indexOfFirstQuestion, indexOfLastQuestion);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  // Render page numbers
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(filteredQuestions.length / questionsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+
 
   return (
     <>
       <Navbar />
-      <main>main</main>
+      <main>
+        <h1>Product Management Interview Questions</h1>
+        <p>Browse 1000+ questions from top tech companies</p>
+        <div className='tag-button-container'>{renderTags()}</div>
+        <div className='question-section'>
+          {currentQuestions.map((question, index) => (
+            <div key={index} className='question'>
+              <h3>{question.question}</h3>
+              {question.tags.map((tag, i) => (
+                <span key={i} className="tag">{tag}</span>
+              ))}
+            </div>
+          ))}
+        </div>
+        {/* Pagination */}
+      <div className="pagination">
+        <span>
+          Showing {indexOfFirstQuestion + 1} - {Math.min(indexOfLastQuestion, filteredQuestions.length)} of {filteredQuestions.length} questions
+        </span>
+        <ul className="page-numbers">
+          {pageNumbers.map(number => (
+            <li key={number} >
+              <button onClick={() => paginate(number)} className={`pagination-number-button ${number === currentPage ? 'active' : ''}`}>{number}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      </main>
       <Footer />
     </>
   )
---
+++ src/data.js
@@ -0,0 +1,191 @@
+const questions = [
+    {
+        "question": "How can we improve user engagement on our platform?",
+        "tags": [
+            "product design",
+            "analytics"
+        ]
+    },
+    {
+        "question": "Explain the difference between TCP and UDP protocols.",
+        "tags": [
+            "technical"
+        ]
+    },
+    {
+        "question": "Describe a situation where you had to resolve a conflict within a team.",
+        "tags": [
+            "behavioral"
+        ]
+    },
+    {
+        "question": "Estimate the number of smartphones sold worldwide in 2023.",
+        "tags": [
+            "guesstimates"
+        ]
+    },
+    {
+        "question": "How would you design a scalable microservices architecture?",
+        "tags": [
+            "system design",
+            "technical"
+        ]
+    },
+    {
+        "question": "What is your approach to defining product strategy?",
+        "tags": [
+            "product strategy"
+        ]
+    },
+    {
+        "question": "How can we improve user engagement on our platform?",
+        "tags": [
+            "product design",
+            "analytics"
+        ]
+    },
+    {
+        "question": "Explain the difference between TCP and UDP protocols.",
+        "tags": [
+            "technical"
+        ]
+    },
+    {
+        "question": "Describe a situation where you had to resolve a conflict within a team.",
+        "tags": [
+            "behavioral"
+        ]
+    },
+    {
+        "question": "Estimate the number of smartphones sold worldwide in 2023.",
+        "tags": [
+            "guesstimates"
+        ]
+    },
+    {
+        "question": "How would you design a scalable microservices architecture?",
+        "tags": [
+            "system design",
+            "technical"
+        ]
+    },
+    {
+        "question": "What is your approach to defining product strategy?",
+        "tags": [
+            "product strategy"
+        ]
+    },
+    {
+        "question": "How can we improve user engagement on our platform?",
+        "tags": [
+            "product design",
+            "analytics"
+        ]
+    },
+    {
+        "question": "Explain the difference between TCP and UDP protocols.",
+        "tags": [
+            "technical"
+        ]
+    },
+    {
+        "question": "Describe a situation where you had to resolve a conflict within a team.",
+        "tags": [
+            "behavioral"
+        ]
+    },
+    {
+        "question": "Estimate the number of smartphones sold worldwide in 2023.",
+        "tags": [
+            "guesstimates"
+        ]
+    },
+    {
+        "question": "How would you design a scalable microservices architecture?",
+        "tags": [
+            "system design",
+            "technical"
+        ]
+    },
+    {
+        "question": "What is your approach to defining product strategy?",
+        "tags": [
+            "product strategy"
+        ]
+    },
+    {
+        "question": "How can we improve user engagement on our platform?",
+        "tags": [
+            "analytics"
+        ]
+    },
+    {
+        "question": "Explain the difference between TCP and UDP protocols.",
+        "tags": [
+            "technical"
+        ]
+    },
+    {
+        "question": "Describe a situation where you had to resolve a conflict within a team.",
+        "tags": [
+            "behavioral"
+        ]
+    },
+    {
+        "question": "Estimate the number of smartphones sold worldwide in 2023.",
+        "tags": [
+            "guesstimates"
+        ]
+    },
+    {
+        "question": "How would you design a scalable microservices architecture?",
+        "tags": [
+            "system design",
+            "technical"
+        ]
+    },
+    {
+        "question": "What is your approach to defining product strategy?",
+        "tags": [
+            "product strategy"
+        ]
+    },
+    {
+        "question": "How can we improve user engagement on our platform?",
+        "tags": [
+            "product design"
+        ]
+    },
+    {
+        "question": "Explain the difference between TCP and UDP protocols.",
+        "tags": [
+            "technical"
+        ]
+    },
+    {
+        "question": "Describe a situation where you had to resolve a conflict within a team.",
+        "tags": [
+            "behavioral"
+        ]
+    },
+    {
+        "question": "Estimate the number of smartphones sold worldwide in 2023.",
+        "tags": [
+            "guesstimates"
+        ]
+    },
+    {
+        "question": "How would you design a scalable microservices architecture?",
+        "tags": [
+            "system design",
+            "technical"
+        ]
+    },
+    {
+        "question": "What is your approach to defining product strategy?",
+        "tags": [
+            "product strategy"
+        ]
+    }
+]
+export default questions;
